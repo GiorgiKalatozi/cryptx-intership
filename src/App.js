79278ccbbc506cf7c26384411fc581a483faf3c0
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-04-27 22:31:49 +0400

    sort func

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Heading from "./Components/Heading";
 import Table from "./Components/Table";
 import Cards from "./Components/Cards";
 import Filters from "./Components/Filters";
+import Demo from "./Components/Demo";
 
 const url =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d";
@@ -44,6 +45,7 @@ function App() {
         search={search}
         setSearch={setSearch}
       />
+      <Demo coins={filteredCoins} setCoins={setCoins} />
       <Table coins={filteredCoins} setCoins={setCoins} />
     </>
   );
