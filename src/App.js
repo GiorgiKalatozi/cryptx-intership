e46207f97feb5db42ff49d761158e03a32e1aefd
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-04-28 04:09:22 +0400

    done

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Heading from "./Components/Heading";
 import Table from "./Components/Table";
 import Cards from "./Components/Cards";
 import Filters from "./Components/Filters";
-import Demo from "./Components/Demo";
-import Fight from "./Components/Fight";
 
 const url =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d";
@@ -46,9 +44,7 @@ function App() {
         search={search}
         setSearch={setSearch}
       />
-      <Fight coins={filteredCoins} />
-      {/* <Demo coins={filteredCoins} setCoins={setCoins} /> */}
-      {/* <Table coins={filteredCoins} setCoins={setCoins} /> */}
+      <Table coins={filteredCoins} />
     </>
   );
 }
--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,118 +1,377 @@
-import { styled } from "@mui/material/styles";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
 import Paper from "@mui/material/Paper";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.white,
-    color: theme.palette.common.black,
-    fontWeight: 700,
-    fontSize: 14,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
+import { visuallyHidden } from "@mui/utils";
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
+function createData(name) {
+  return {
+    name,
+  };
+}
 
-function CustomizedTables({ coins }) {
+const rows = [
+  createData("#"),
+  createData("Coin"),
+  createData(""),
+  createData("Price"),
+  createData("1h"),
+  createData("24h"),
+  createData("7d"),
+  createData("24h Volume	"),
+  createData("Mkt Cap"),
+];
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  {
+    id: "market_cap_rank",
+    numeric: false,
+    disablePadding: true,
+    label: "#",
+  },
+  {
+    id: "name",
+    numeric: true,
+    disablePadding: false,
+    label: "Coin",
+  },
+  {
+    id: "symbol",
+    numeric: true,
+    disablePadding: false,
+    label: "",
+  },
+  {
+    id: "current_price",
+    numeric: true,
+    disablePadding: false,
+    label: "Price",
+  },
+  {
+    id: "price_change_percentage_1h_in_currency",
+    numeric: true,
+    disablePadding: false,
+    label: "1h",
+  },
+  {
+    id: "price_change_percentage_24h_in_currency",
+    numeric: true,
+    disablePadding: false,
+    label: "24h",
+  },
+  {
+    id: "price_change_percentage_7d_in_currency",
+    numeric: true,
+    disablePadding: false,
+    label: "7d",
+  },
+  {
+    id: "total_volume",
+    numeric: true,
+    disablePadding: false,
+    label: "24h Volume",
+  },
+  {
+    id: "market_cap",
+    numeric: true,
+    disablePadding: false,
+    label: "Mkt Cap",
+  },
+];
+
+function EnhancedTableHead(props) {
+  const { order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>#</StyledTableCell>
-            <StyledTableCell>Coin</StyledTableCell>
-            <StyledTableCell></StyledTableCell>
-            <StyledTableCell align="right">Price</StyledTableCell>
-            <StyledTableCell align="right">1h</StyledTableCell>
-            <StyledTableCell align="right">24h</StyledTableCell>
-            <StyledTableCell align="right">7d</StyledTableCell>
-            <StyledTableCell align="right">24h Volume</StyledTableCell>
-            <StyledTableCell align="right">Mkt Cap</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {coins.map((coin) => (
-            <StyledTableRow key={coin.id}>
-              <StyledTableCell align="left">
-                {coin.market_cap_rank}
-              </StyledTableCell>
-              <StyledTableCell
-                className="img-symbol"
-                component="th"
-                scope="row"
-              >
-                <div className="coin-symbol">
-                  <img src={coin.image} alt={coin.name} />
-                  <p className="coin-name"> {coin.name}</p>
-                </div>
-              </StyledTableCell>
-              <StyledTableCell align="left">
-                {coin.symbol.toUpperCase()}
-              </StyledTableCell>
-              <StyledTableCell align="right">
-                ${coin.current_price.toLocaleString()}
-              </StyledTableCell>
-              <StyledTableCell align="right">
-                {coin.price_change_percentage_1h_in_currency.toFixed(2) < 0 ? (
-                  <p className="red">
-                    {coin.price_change_percentage_1h_in_currency.toFixed(2)}%
-                  </p>
-                ) : (
-                  <p className="green">
-                    {coin.price_change_percentage_1h_in_currency.toFixed(2)}%
-                  </p>
-                )}
-              </StyledTableCell>
-              <StyledTableCell align="right">
-                {coin.price_change_percentage_24h_in_currency.toFixed(2) < 0 ? (
-                  <p className="red">
-                    {coin.price_change_percentage_24h_in_currency.toFixed(2)}%
-                  </p>
-                ) : (
-                  <p className="green">
-                    {coin.price_change_percentage_24h_in_currency.toFixed(2)}%
-                  </p>
-                )}
-              </StyledTableCell>
-              <StyledTableCell align="right">
-                {coin.price_change_percentage_7d_in_currency.toFixed(2) < 0 ? (
-                  <p className="red">
-                    {coin.price_change_percentage_7d_in_currency.toFixed(2)}%
-                  </p>
-                ) : (
-                  <p className="green">
-                    {coin.price_change_percentage_7d_in_currency.toFixed(2)}%
-                  </p>
-                )}
-              </StyledTableCell>
-              <StyledTableCell align="right">
-                ${coin.total_volume.toLocaleString()}
-              </StyledTableCell>
-              <StyledTableCell align="right">
-                ${coin.market_cap.toLocaleString()}
-              </StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <TableHead>
+      <TableRow>
+        {/* <TableCell padding="checkbox">#</TableCell> */}
+        {headCells.map((headCell) => (
+          <TableCell
+            style={{ fontWeight: "bolder" }}
+            key={headCell.id}
+            // align={headCell.numeric ? "right" : "left"}
+            align="left"
+            // padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
   );
 }
 
-export default CustomizedTables;
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+export default function EnhancedTable({ coins }) {
+  const [order, setOrder] = useState("asc");
+  const [orderBy, setOrderBy] = useState("id");
+  const [selected, setSelected] = useState([]);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mb: 2 }}>
+        <TableContainer>
+          <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {stableSort(coins, getComparator(order, orderBy)).map(
+                (coin, index) => {
+                  const isItemSelected = isSelected(coin.name);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, coin.id)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={coin.name}
+                      selected={isItemSelected}
+                    >
+                      <TableCell
+                        align="left"
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {coin.market_cap_rank}
+                      </TableCell>
+                      <TableCell
+                        align="left"
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        <div className="coin-symbol">
+                          <img src={coin.image} alt={coin.name} />
+                          <p className="coin-name"> {coin.name}</p>
+                        </div>
+                      </TableCell>
+                      <TableCell
+                        align="left"
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {coin.symbol.toUpperCase()}
+                      </TableCell>
+                      <TableCell
+                        align="left"
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        ${coin.current_price.toLocaleString()}
+                      </TableCell>
+                      <TableCell
+                        align="left"
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {coin.price_change_percentage_1h_in_currency.toFixed(
+                          2
+                        ) < 0 ? (
+                          <p className="red">
+                            {coin.price_change_percentage_1h_in_currency.toFixed(
+                              2
+                            )}
+                            %
+                          </p>
+                        ) : (
+                          <p className="green">
+                            {coin.price_change_percentage_1h_in_currency.toFixed(
+                              2
+                            )}
+                            %
+                          </p>
+                        )}
+                      </TableCell>
+                      <TableCell
+                        align="left"
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {coin.price_change_percentage_24h_in_currency.toFixed(
+                          2
+                        ) < 0 ? (
+                          <p className="red">
+                            {coin.price_change_percentage_24h_in_currency.toFixed(
+                              2
+                            )}
+                            %
+                          </p>
+                        ) : (
+                          <p className="green">
+                            {coin.price_change_percentage_24h_in_currency.toFixed(
+                              2
+                            )}
+                            %
+                          </p>
+                        )}
+                      </TableCell>
+                      <TableCell
+                        align="left"
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {coin.price_change_percentage_7d_in_currency.toFixed(
+                          2
+                        ) < 0 ? (
+                          <p className="red">
+                            {coin.price_change_percentage_7d_in_currency.toFixed(
+                              2
+                            )}
+                            %
+                          </p>
+                        ) : (
+                          <p className="green">
+                            {coin.price_change_percentage_7d_in_currency.toFixed(
+                              2
+                            )}
+                            %
+                          </p>
+                        )}
+                      </TableCell>
+                      <TableCell
+                        align="left"
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        ${coin.total_volume.toLocaleString()}
+                      </TableCell>
+                      <TableCell
+                        align="left"
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        ${coin.market_cap.toLocaleString()}
+                      </TableCell>
+                    </TableRow>
+                  );
+                }
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    </Box>
+  );
+}
